17e2f87d8b506f227cc6770f5c908378a0fbaf58
Milan <wkmswanthra@gmail.com>
2025-01-16 01:42:25 +0530

    Reading incoming request data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use std::net::TcpListener;
-
+use std::net::TcpStream;
+use std::io::prelude::*;
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming(){
-        let stream = stream.unwrap();
+        let  stream= stream.unwrap();
 
-        print!("Connected to {}", stream.peer_addr().unwrap());
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..])
+    )
 }
